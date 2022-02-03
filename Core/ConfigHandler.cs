ac26b98b8a6c68fd470d0ee22916bbd9fb0d81bb
Ventriix (Nico) <ventriix@variatystudios.dev>
2022-02-03 22:29:45 +0100

    Added auto updater

---
+++ Core/ConfigHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Config.Net;
+
+namespace Ninance_v2.Core
+{
+
+    public interface INinanceSettings
+    {
+        [Option(Alias = "General.UsingDarkMode")]
+        bool UsingDarkMode { get; set; }
+    }
+
+    public class ConfigHandler
+    {
+
+        public INinanceSettings Settings { get; }
+
+        public ConfigHandler()
+        {
+            string path = Environment.CurrentDirectory + "/data/settings.ini";
+
+            if (!File.Exists(path))
+                File.Create(path).Close();
+
+            Settings = new ConfigurationBuilder<INinanceSettings>()
+                .UseIniFile(path)
+                .Build();
+        }
+    }
+}
